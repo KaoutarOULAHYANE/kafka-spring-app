4df1821d0fd6de7e94e7621e9b88098a3d740b85
OULAHYANE <kaoutar.oulahyane@gmail.com>
2021-01-11 16:21:14 +0100

    first commit

---
+++ src/test/java/org/sid/kafkaappspring/KafkaAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.kafkaappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
